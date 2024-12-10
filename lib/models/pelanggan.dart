7a9023e28d244b13998b076e571e5d4f5bcf088d
Rechardo-02 <rechardokusuma@gmail.com>
2024-12-10 22:12:52 +0700

    this fris commit

--- lib/models/pelanggan.dart
+++ lib/models/pelanggan.dart
@@ -1,9 +1,22 @@
 class Pelanggan {
-  final String kode;
-  final String nama;
+  
+  final String kode; 
+  final String nama; 
 
+  
   const Pelanggan({
     required this.kode,
     required this.nama,
   });
+
+  
+  @override
+  String toString() {
+    return '''
+    ======================
+    Kode Pelanggan : $kode
+    Nama Pelanggan : $nama
+    ======================
+    ''';
+  }
 }
